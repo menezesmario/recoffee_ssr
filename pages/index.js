502d61ce3eca149cd997403132d565afa6f004d3
Mário Menezes <mario082@gmail.com>
2021-02-12 18:07:18 -0300

    toggle menu

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head';
+import { Navbar } from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>reCoffee</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Navbar />
-      <div>Hello World</div>
+      <div>ShopBlog</div>
     </div>
   );
 }
