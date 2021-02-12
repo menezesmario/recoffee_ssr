de83c801ca6b68ce256e8b555b297a27e3ff13b1
Mário Menezes <mario082@gmail.com>
2021-02-12 20:13:19 -0300

    hero

---
+++ components/Banner.jsx
@@ -0,0 +1,39 @@
+// import Link from "next/link";
+// import { useState } from "react";
+// import styles from "../styles/Banner.module.css";
+export const Banner = () => {
+  // const [active, setActive] = useState(false);
+
+  // const handleClick = () => {
+  //   setActive(!active);
+  // };
+
+  return (
+    <main
+      className="h-screen flex items-center px-6 lg:px-32 bg-yellow-900 text-white relative"
+      style={{
+        backgroundImage: `url("https://pausecoffee.co.za/wp-content/uploads/2017/06/shutterstock_289584713_sml.jpg")`, backgroundSize: 'cover', overflow: 'hiden',
+      }}
+    >
+      {/* <video src='./images/coffeebanner.mp4' autoPlay loop muted /> */}
+       
+      <section className="w-full md:w-9/12 xl:w-8/12" style={{marginTop: '40vh'}}>
+        <h1 className="text-3xl lg:text-5xl font-bold text-white">reCoffee</h1>
+        <p className="font-bold mb-1">
+          Os melhores cafés diretamente do produtor
+        </p>
+        <p>Descontos especiais para programadores</p>
+      </section>
+      <footer className="absolute right-0 bottom-0 p-6 lg:p-32">
+        <p className="font-bold mb-1">Mário Menezes</p>
+        <p>Recode Pro 2021</p>
+      </footer>
+    </main>
+  );
+};
+
+//   You could also try setting style inline without using a variable, like so:
+
+// style={{"height" : "100%"}} or,
+
+// for multiple attributes: style={{"height" : "100%", "width" : "50%"}}
