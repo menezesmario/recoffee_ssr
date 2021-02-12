9e8a8da4d8006b5085c09fa6f451703963a05945
Mário Menezes <mario082@gmail.com>
2021-02-12 17:58:07 -0300

    init Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export const Navbar = () => {
+    return (
+      <>
+              <nav className='flex items-center flex-wrap bg-green-300 p-3 '>
+
+
+                  
+              </nav>
+      </>
+    )
+  }
